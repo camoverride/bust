b7571c9d5342b584435fa4d027b6887554561992
camoverride <camoverride@gmail.com>
2026-04-23 14:53:27 -0700

    revert

--- random_looking_test.py
+++ random_looking_test.py
@@ -79,38 +79,7 @@ def draw_eye(
         )
 
 
-
-from picamera2 import Picamera2
-import cv2
-
-# Initialize camera
-picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration())
-picam2.start()
-
-
-
-
-
 while True:
-
-    # Capture frame
-    frame = picam2.capture_array()
-
-    # Rotate 90 degrees clockwise
-    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-
-    # Show frame
-    cv2.imshow("Pi Camera Feed", frame)
-
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-
-
-
     # New random target
     if random.random() < 0.02:
         target_x = random.randint(-10, 10)
@@ -143,7 +112,3 @@ while True:
     device.display(image)
 
     time.sleep(0.03)
-
-
-cv2.destroyAllWindows()
-picam2.stop()
